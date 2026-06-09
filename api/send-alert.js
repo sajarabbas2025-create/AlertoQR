46ccf7da38ebc43f63aad80a138dd26e64af3047
sajarabbas2025-create <sajarabbas2025@gmail.com>
2026-06-09 12:06:59 +0530

    Update send-alert.js

--- api/send-alert.js
+++ api/send-alert.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
     // 2. Temporary Database (Asli app mein hum ise Supabase se jodenge)
     const vehicleDatabase = {
-      "1001": "+917409081112", // YAHAN APNA ASLI TEST NUMBER DAAL DIJIYEGA
+      "1001": "+916388522427", // YAHAN APNA ASLI TEST NUMBER DAAL DIJIYEGA
       "2540": "+918765432109"  
     };
 
