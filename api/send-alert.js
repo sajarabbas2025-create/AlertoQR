1c90587d35f40f027fdb92ada712cc3c888a87d1
sajarabbas2025-create <sajarabbas2025@gmail.com>
2026-06-10 23:11:17 +0530

    Update send-alert.js

--- api/send-alert.js
+++ api/send-alert.js
@@ -37,10 +37,14 @@ export default async function handler(req, res) {
 
     const callApiUrl = `https://restapi.smscountry.com/v0.1/Accounts/${SMSCOUNTRY_AUTH_KEY}/Calls/`;
     
-    // 4. Clean XML Payload (URLs Removed, Using Say instead of Play)
+    // 4. PERFECT PAYLOAD: Dummy URLs wapis lagaye hain validation pass karne ke liye + <Say> tag
     const jsonBodyData = {
         "Number": helperCleanNumber,    
         "CallerId": "918634512424",
+        "RingUrl": "https://alertoqr.in/ring",
+        "AnswerUrl": "https://alertoqr.in/answer",
+        "HangupUrl": "https://alertoqr.in/hangup",
+        "HttpMethod": "POST",
         "Xml": `<Response><Say>Please wait, we are connecting your secure call.</Say><Dial>${ownerCleanNumber}</Dial></Response>` 
     };
 
