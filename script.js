bf9d7c2078427efcd5f9756d986cd87006dc6780
sajarabbas2025-create <sajarabbas2025@gmail.com>
2026-07-06 18:34:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+// ===============================
+// ALERTOQR SCRIPT
+// Part 1
+// ===============================
 
+const scrollTopBtn = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        scrollTopBtn.classList.add("show");
+
+    } else {
+
+        scrollTopBtn.classList.remove("show");
+
+    }
+
+});
+
+scrollTopBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
+
+// Smooth Scroll Menu
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+
+            target.scrollIntoView({
+
+                behavior: "smooth"
+
+            });
+
+        }
+
+    });
+
+});
